b8e0134a028c7b3195a44419c1a3effcbb372937
lukby2457 <kjh7442455@gmail.com>
2024-07-30 10:28:44 +0900

    reset.css 적용 및 css 수정, js에 module 적용 및 js문서 분리

--- js/search.js
+++ js/search.js
@@ -1,18 +1,17 @@
 // 검색어에 따라 card를 표시하는 function
 export function changeDisplay(index, arr1, arr2) {
   let indexFind = String(index.value).toLowerCase();
+  let lowerArr = arr1.map(str => str.toLowerCase());
 
-  let merged = arr1.reduce((obj, title, id) => {
+  let merged = lowerArr.reduce((obj, title, id) => {
     return {...obj, [title] : arr2[id]};
   }, new Object);
 
   let objectArr = Object.entries(merged);
 
-  let lowerArr = objectArr.map(str => str[0].toLowerCase());
-
   if(lowerArr.some(title => title.includes(indexFind))) {
     objectArr.forEach(data => {
-      let title = String(data[0]).toLowerCase();
+      let title = data[0];
       let id = data[1];
   
       if(title.includes(indexFind)) {
